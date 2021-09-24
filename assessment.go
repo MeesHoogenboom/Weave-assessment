b37b3d2052207b1fa9ddb1ac5702ccf523bfe713
Mees <mees_hoogenboom@live.nl>
2021-09-24 13:23:12 +0200

    Added function to check if timestamp is a weekday

--- assessment.go
+++ assessment.go
@@ -7,13 +7,14 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 var i int
 var metering_point_id int
 var reading_type int
 var reading int
-var created_at int
+var created_at int64
 
 func main() {
 	csvReader()
@@ -43,18 +44,40 @@ func csvReader() {
 
 		if i != 1 {
 			if i > 2 {
-				new_reading, _ := strconv.Atoi(record[2])
-				usage := new_reading - reading
-				fmt.Println("usage =", usage)
+				newReading, _ := strconv.Atoi(record[2])
+				// cost := electricityCost(newReading, reading)
+				fmt.Println(newReading)
+				weekday(created_at)
+
 			}
 			metering_point_id, _ = strconv.Atoi(record[0])
 			reading_type, _ = strconv.Atoi(record[1])
 			reading, _ = strconv.Atoi(record[2])
-			created_at, _ = strconv.Atoi(record[3])
+			created_at, _ = strconv.ParseInt(record[3], 10, 64)
 
 			fmt.Println("new reading =", reading)
 			// fmt.Println(metering_point_id, reading_type, reading, created_at)
 		}
 
 	}
+}
+
+// func electricityCost(newReading int, oldReading int) {
+// 	usage := newReading - oldReading
+// 	if usage > 100 || usage < 0 {
+
+// 	}
+// }
+
+func weekday(created_at int64) bool {
+	var isWeekday bool
+	day := int(time.Unix(created_at, 0).Weekday())
+
+	if day != 0 || day != 6 {
+		isWeekday = true
+	} else {
+		isWeekday = false
+	}
+
+	return isWeekday
 }
